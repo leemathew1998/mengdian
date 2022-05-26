7d2531f42473b02786afe771bac6e6b81dc993dd
heyf <549400859@qq.com>
2022-05-26 14:20:34 +0800

    业绩页面

---
+++ src/components/tables/Tabless.vue
@@ -0,0 +1,92 @@
+<template>
+	<div class="warp">
+		<a-table :columns="columns"
+			:data-source="data" bordered @change="pagechange" :loading="tableLoading" :pagination="false"
+			>
+			<!-- <template > -->
+			<div slot="orderStatus" slot-scope="text, record" class='orderStatus'> {{text}}</div>
+			<!-- </template> -->
+			<template slot="operation" slot-scope="text, record">
+				<div>
+					<slot :table_key="record"></slot>
+				</div>
+			</template>
+		</a-table>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Tables',
+		props: {
+			columns: {
+				type: Array,
+				required: true
+			},
+			data: {
+				type: Array,
+				required: true
+			},
+			operationName: {
+				type: String,
+				default: '',
+			},
+			tableLoading: {
+				type: Boolean
+			},
+		},
+		data() {
+			return {
+				selectedRowKeys: []
+			}
+		},
+		created() {
+			if (this.operationName !== '' && this.columns[this.columns.length - 1].dataIndex !== 'operation') {
+				this.columns.push({
+					title: '操作',
+					dataIndex: 'operation',
+					scopedSlots: {
+						customRender: 'operation'
+					},
+				})
+			}
+			// console.log('created')
+		},
+
+		mounted() {
+			// console.log('mounted')
+			// let status = document.getElementsByClassName('orderStatus')
+			// for (var i = 0; i < status.length; i++) {
+			// 	// console.log(status[i].parentNode)
+			// 	// status[i].parentNode.style['backgroundColor'] = '#149BD5'
+			// 	status[i].parentElement.style.background='#149BD5'
+			// }
+			// let status = document.getElementsByClassName('orderStatus')
+			// for (var i = 0; i < status.length; i++) {
+			// 	// status[i].parentNode.style['backgroundColor'] = '#149BD5'
+			// 	status[i].innerHTML = 'asas'+i
+			// }
+		},
+		methods: {
+			pagechange(e) {
+				// let status = document.getElementsByClassName('orderStatus')
+				// console.log(status[0])
+				// for (var i = 0; i < status.length; i++) {
+				// 	// status[i].style['backgroundColor'] = '#149BD5'
+				// 	status[i].innerHTML = 'asas'+i
+				// 	// status[i].parentNode.classList.add("parentNode")
+				// 	// console.log(status[i].parentNode)
+				// }
+				// console.log(status[0])
+			},
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.warp {
+		width: 100%;
+
+	}
+</style>
